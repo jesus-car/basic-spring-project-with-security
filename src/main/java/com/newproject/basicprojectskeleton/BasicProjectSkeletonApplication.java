c42017c92ef94fb0ec8562401af6d240940add86
jesus Garcia <jesus.garcia2611@hotmail.com>
2024-10-03 19:02:26 -0500

    first commit

---
+++ src/main/java/com/newproject/basicprojectskeleton/BasicProjectSkeletonApplication.java
@@ -0,0 +1,13 @@
+package com.newproject.basicprojectskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicProjectSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicProjectSkeletonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newproject/basicprojectskeleton/BasicProjectSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newproject.basicprojectskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicProjectSkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
